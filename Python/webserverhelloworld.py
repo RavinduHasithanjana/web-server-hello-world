a7b059cfb564804d72da839c115492203960edf3
RavinduHasithanjana <ravinduceylofana@outlook.com>
2018-04-23 22:29:01 +0530

    did changes

--- Python/webserverhelloworld.py
+++ Python/webserverhelloworld.py
@@ -5,7 +5,7 @@ PORT = 8000
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-httpd = SocketServer.TCPServer(("Hello world", PORT), Handler)
+httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print "serving at port", PORT
 httpd.serve_forever()
